d152bd42f0cd1b498fa3a8cb4822a99f2f3cdc0a
LeanderSB <leander.borges1@gmail.com>
2024-09-10 22:39:36 -0300

    Add files via upload

---
+++ aula2_questão2.py
@@ -0,0 +1,16 @@
+#2) Transforme o seguinte algoritmo em código. Lembre-se das funções print e type que já conhecemos de forma geral.
+#Armazene o texto "o resultado é"  em uma variável, o valor 10 em outra variável, e o valor 3.5 numa terceira variável.
+#Some os valores da segunda e terceira variável e armazene em outra variável.
+#Imprima todas as variáveis na ordem de criação e imprima também seus tipos.
+R="O resultado é:"
+x=10
+y=3.5
+soma=x+y
+print(R)
+print(x)
+print(y)
+print(soma)
+print(type(R))
+print(type(x))
+print(type(y))
+print(type(soma))
